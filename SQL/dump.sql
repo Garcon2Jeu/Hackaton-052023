bde18f7daf7ef1ba5769c9c3dc7ca540958d5ef9
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-05-11 15:03:24 +0200

    Populate BDD

--- SQL/dump.sql
+++ SQL/dump.sql
@@ -1,23 +1,31 @@
 -- Active: 1682060498345@@127.0.0.1@3306@shiporent
 
+DROP TABLE ship_planet;
+
+DROP TABLE ship;
+
+DROP TABLE planet;
+
 CREATE TABLE
     `planet` (
-        `id` int NOT NULL,
+        `id` int NOT NULL AUTO_INCREMENT,
         `Name` VARCHAR(80) NOT NULL,
         `description` TEXT NOT NULL,
         `distance_from_earth` iNT NOT NULL,
+        `picturePath` VARCHAR(255) NOT NULL,
         PRIMARY KEY (`id`)
     );
 
 CREATE TABLE
     `ship` (
-        `id` int NOT NULL,
+        `id` int NOT NULL AUTO_INCREMENT,
         `model` VARCHAR(255) NOT NULL,
         `cost_in_credit` INT NOT NULL,
         `hyperdrive_rating` FLOAT NOT NULL,
         `crew` INT NOT NULL,
         `passenger` INT NOT NULL,
         `cargo_capacity` INT NOT NULL,
+        `picturePath` VARCHAR(255) NOT NULL,
         PRIMARY KEY (`id`)
     );
 
---
+++ src/Model/JsonDecoderManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+use App\Model\AbstractManager;
+
+class JsonDecoderManager extends AbstractManager
+{
+    public function insertShips(array $shipData)
+    {
+        $query = "INSERT INTO ship (
+        model, 
+        cost_in_credits, 
+        hyperdrive_rating, 
+        crew, 
+        passengers, 
+        cargo_capacity, 
+        picturePath
+        ) VALUES ( 
+        :model,
+        :cost_in_credits,
+        :hyperdrive_rating,
+        :crew,
+        :passengers,
+        :cargo_capacity,
+        :picturePath
+        )";
+
+        $statement = $this->pdo->prepare($query);
+
+        $statement->bindValue(":model", $shipData["model"], PDO::PARAM_STR);
+        $statement->bindValue(":cost_in_credits", $shipData["cost_in_credits"], PDO::PARAM_INT);
+        $statement->bindValue(":hyperdrive_rating", $shipData["hyperdrive_rating"], PDO::PARAM_STR);
+        $statement->bindValue(":crew", $shipData["crew"], PDO::PARAM_INT);
+        $statement->bindValue(":passengers", $shipData["passengers"], PDO::PARAM_INT);
+        $statement->bindValue(":cargo_capacity", $shipData["cargo_capacity"], PDO::PARAM_INT);
+        $statement->bindValue(":picturePath", $shipData["picturePath"], PDO::PARAM_STR);
+
+        $statement->execute();
+    }
+
+    public function insertPlanets(array $planetData)
+    {
+        $query = "INSERT INTO planet (
+            name, 
+            description, 
+            distance_from_earth, 
+            picturePath 
+            ) VALUES ( 
+            :name, 
+            :description, 
+            :distance_from_earth, 
+            :picturePath 
+            )";
+
+        $statement = $this->pdo->prepare($query);
+
+        $statement->bindValue(":name", $planetData["name"], PDO::PARAM_STR);
+        $statement->bindValue(":description", $planetData["description"], PDO::PARAM_STR);
+        $statement->bindValue(":distance_from_earth", $planetData["distance_from_earth"], PDO::PARAM_INT);
+        $statement->bindValue(":picturePath", $planetData["picturePath"], PDO::PARAM_STR);
+
+        $statement->execute();
+    }
+}
